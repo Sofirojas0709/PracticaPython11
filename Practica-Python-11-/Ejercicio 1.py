4dd35b9c9e6b074260bbf862df8b88298fc3dbbb
Sofirojas0709 <m.rojas@coadvil.edu.co>
2025-03-04 11:11:36 -0500

    Add files via upload

---
+++ Practica-Python-11-/Ejercicio 1.py
@@ -0,0 +1,13 @@
+Dia1 = float(input("¿Cuantas horas vuela la nave en día 1?"))
+Dia2 = float(input("¿Cuantas horas vuela la nave en día 2?"))
+Dia3 = float(input("¿Cuantas horas vuela la nave en día 3?"))
+Dia4 = float(input("¿Cuantas horas vuela la nave en día 4?"))
+Dia5 = float(input("¿Cuantas horas vuela la nave en día 5?"))
+Dia6 = float(input("¿Cuantas horas vuela la nave en día 6?"))
+Dia7 = float(input("¿Cuantas horas vuela la nave en día 7?"))
+Dia8 = float(input("¿Cuantas horas vuela la nave en día 8?"))
+Dia9 = float(input("¿Cuantas horas vuela la nave en día 9?"))
+Dia10 = float(input("¿Cuantas horas vuela la nave en día 10?"))
+suma = Dia1 + Dia2 + Dia3 + Dia4 + Dia5 + Dia6 + Dia7 + Dia8 + Dia9 + Dia10
+promedio = suma / 10
+print(f"El promedio de horas de vuelo es: {promedio}")
